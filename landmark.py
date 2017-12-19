0e6eaf0d62a43e80b611b160b46963c7ef0a73e5
yinguobing <yinguobing@gmail.com>
2017-12-19 17:53:28 +0800

    add model function

---
+++ landmark.py
@@ -0,0 +1,192 @@
+"""
+Convolutional Neural Network Estimator for facial landmark detection.
+"""
+
+import tensorflow as tf
+
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNEL = 3
+
+
+def cnn_model_fn(features, labels, mode):
+    """
+    The model function for the network.
+    """
+    # |== Layer 0: input layer ==|
+    # Input feature x should be of shape (batch_size, image_width, image_height, color_channels).
+    # Image shape should be checked for safety reasons at early stages, and could be removed
+    # before training actually starts.
+    assert features['x'].shape[1:] == (
+        IMG_WIDTH, IMG_HEIGHT, IMG_CHANNEL), "Image size does not match."
+    inputs = tf.to_float(features['x'], name="input_to_float")
+
+    # |== Layer 1 ==|
+
+    # Convolutional layer.
+    # Computes 32 features using a 3x3 filter with ReLU activation.
+    conv1 = tf.layers.conv2d(
+        inputs=inputs,
+        filters=32,
+        kernel_size=[3, 3],
+        strides=(1, 1),
+        padding='valid',
+        activation=tf.nn.relu)
+
+    # Pooling layer.
+    # First max pooling layer with a 2x2 filter and stride of 2.
+    pool1 = tf.layers.max_pooling2d(
+        inputs=conv1,
+        pool_size=[2, 2],
+        strides=(2, 2),
+        padding='valid')
+
+    # |== Layer 2 ==|
+
+    # Convolutional layer
+    # Computes 64 features using a 3x3 filter with ReLU activation.
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[3, 3],
+        strides=(1, 1),
+        padding='valid',
+        activation=tf.nn.relu)
+
+    # Convolutional layer
+    # Computes 64 features using a 3x3 filter with ReLU activation.
+    conv3 = tf.layers.conv2d(
+        inputs=conv2,
+        filters=64,
+        kernel_size=[3, 3],
+        strides=(1, 1),
+        padding='valid',
+        activation=tf.nn.relu)
+
+    # Pooling layer
+    # Second max pooling layer with a 2x2 filter and stride of 2.
+    pool2 = tf.layers.max_pooling2d(
+        inputs=conv3,
+        pool_size=[2, 2],
+        strides=(2, 2),
+        padding='valid')
+
+    # |== Layer 3 ==|
+
+    # Convolutional layer
+    # Computes 64 features using a 3x3 filter with ReLU activation.
+    conv4 = tf.layers.conv2d(
+        inputs=pool2,
+        filters=64,
+        kernel_size=[3, 3],
+        strides=(1, 1),
+        padding='valid',
+        activation=tf.nn.relu)
+
+    # Convolutional layer
+    # Computes 64 features using a 3x3 filter with ReLU activation.
+    conv5 = tf.layers.conv2d(
+        inputs=conv4,
+        filters=64,
+        kernel_size=[3, 3],
+        strides=(1, 1),
+        padding='valid',
+        activation=tf.nn.relu)
+
+    # Pooling layer
+    # Third max pooling layer with a 2x2 filter and stride of 2.
+    pool3 = tf.layers.max_pooling2d(
+        inputs=conv5,
+        pool_size=[2, 2],
+        strides=(2, 2),
+        padding='valid')
+
+    # |== Layer 4 ==|
+
+    # Convolutional layer
+    # Computes 128 features using a 3x3 filter with ReLU activation.
+    conv6 = tf.layers.conv2d(
+        inputs=pool3,
+        filters=128,
+        kernel_size=[3, 3],
+        strides=(1, 1),
+        padding='valid',
+        activation=tf.nn.relu)
+
+    # Convolutional layer
+    # Conputes 128 features using a 3x3 filter with ReLU activation.
+    conv7 = tf.layers.conv2d(
+        inputs=conv6,
+        filters=128,
+        kernel_size=[3, 3],
+        strides=(1, 1),
+        padding='valid',
+        activation=tf.nn.relu)
+
+    # Pooling layer
+    # Fourth max pooling layer with a 2x2 filter and stride of 2.
+    pool4 = tf.layers.max_pooling2d(
+        inputs=conv7,
+        pool_size=[2, 2],
+        strides=(1, 1),
+        padding='valid')
+
+    # |== Layer 5 ==|
+
+    # Convolutional layer
+    conv8 = tf.layers.conv2d(
+        inputs=pool4,
+        filters=256,
+        kernel_size=[3, 3],
+        strides=(1, 1),
+        padding='valid',
+        activation=tf.nn.relu)
+
+    # |== Layer 6 ==|
+
+    # Flatten tensor into a batch of vectors
+    flatten = tf.layers.flatten(inputs=conv8)
+
+    # Dense layer 1, a fully connected layer.
+    dense1 = tf.layers.dense(
+        inputs=flatten,
+        units=1024,
+        activation=tf.nn.relu,
+        use_bias=True)
+
+    # Dense layer 2, also known as the output layer.
+    logits = tf.layers.dense(
+        inputs=dense1,
+        units=136,
+        activation=None,
+        use_bias=True,
+        name="logits")
+
+    # Make prediction for PREDICATION mode.
+    predictions_dict = {
+        "name": features['name'],
+        "logits": logits
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions_dict)
+
+    # Caculate loss using minimal squared error.
+    label_tensor = tf.convert_to_tensor(labels, dtype=tf.float32)
+    loss = tf.losses.mean_pairwise_squared_error(
+        labels=label_tensor, predictions=logits)
+
+    # Configure the train OP for TRAIN mode.
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "MSE": tf.metrics.root_mean_squared_error(
+            labels=label_tensor,
+            predictions=logits)}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
